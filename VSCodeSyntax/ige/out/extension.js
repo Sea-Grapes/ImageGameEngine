7ffe028ddcf429f3061b6e59136d3a0c1901c83f
Sea Grapes <natuloid@gmail.com>
2025-02-28 20:15:36 -0800

    working launch task

---
+++ VSCodeSyntax/ige/out/extension.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+const vscode = require("vscode");
+function activate() {
+    vscode.window.showInformationMessage('this is a test');
+}
+//# sourceMappingURL=extension.js.map
