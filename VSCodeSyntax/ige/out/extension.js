9b2eb3c7999b6cf80b14c32f2d30fd49db146e13
Sea Grapes <natuloid@gmail.com>
2025-02-28 20:30:08 -0800

    update

--- VSCodeSyntax/ige/out/extension.js
+++ VSCodeSyntax/ige/out/extension.js
@@ -1,8 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = activate;
-const vscode = require("vscode");
-function activate() {
-    vscode.window.showInformationMessage('this is a test');
+exports.deactivate = deactivate;
+const path = require("path");
+const vscode_1 = require("vscode");
+const node_1 = require("vscode-languageclient/node");
+let client;
+function activate(context) {
+    vscode_1.window.showInformationMessage('ige enabled');
+    const module = context.asAbsolutePath(path.join('out', 'server.js'));
+    const serverOptions = {
+        run: { module, transport: node_1.TransportKind.ipc },
+        debug: { module, transport: node_1.TransportKind.ipc }
+    };
+    // this uses `languages/id` from package.json
+    const clientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'ige' }],
+        synchronize: { fileEvents: vscode_1.workspace.createFileSystemWatcher('**/*.ige') }
+    };
+    client = new node_1.LanguageClient('ige', serverOptions, clientOptions);
+    client.start();
+}
+function deactivate() {
+    if (client)
+        client.stop();
 }
 //# sourceMappingURL=extension.js.map
--- VSCodeSyntax/ige/src/extension.ts
+++ VSCodeSyntax/ige/src/extension.ts
@@ -1,5 +1,36 @@
-import * as vscode from 'vscode'
+import * as path from 'path'
+import { workspace, ExtensionContext, window } from 'vscode'
 
-export function activate() {
-  vscode.window.showInformationMessage('this is a test')
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient/node'
+
+let client: LanguageClient
+
+export function activate(context: ExtensionContext) {
+  window.showInformationMessage('ige enabled')
+
+  const module = context.asAbsolutePath(path.join('out', 'server.js'))
+
+  const serverOptions: ServerOptions = {
+    run: { module, transport: TransportKind.ipc },
+    debug: { module, transport: TransportKind.ipc }
+  }
+
+  // this uses `languages/id` from package.json
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'ige' }],
+    synchronize: { fileEvents: workspace.createFileSystemWatcher('**/*.ige')}
+  }
+
+  client = new LanguageClient('ige', serverOptions, clientOptions)
+
+  client.start()
+}
+
+export function deactivate() {
+  if(client) client.stop()
 }
